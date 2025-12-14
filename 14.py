9b91f88384e4cbfec071f5070ec276179139dcab
Alok-Saini11 <alok23550364@gmail.com>
2025-12-14 20:20:42 +0530

    Task 14

--- 14.py
+++ 14.py
@@ -10,6 +10,17 @@ content = file.read()
 print(content)
 file.close()
 
+# Readline
+with open("demo.txt") as file:
+    content = file.readline()
+    print(content)
+
+# Readlines
+with open("demo.txt") as file:
+    content = file.readlines()
+    print(content)    
+
+
 # APPEND
 file = open("demo.txt","a")
 file.write("Hello i am alok")
@@ -19,13 +30,22 @@ file = open("demo.txt","r")
 print(file.read())
 file.close()
 
-# SECOND METHOD TO WRITE IN A FILE
+# Write
 with open("demo.txt","w")as file:
     print(file.write("my name is ram"))
 
 with open("demo.txt","r")as file:
-  
   print(file.read())
+
+# Writelines
+line = [
+    "This is first line"
+    "This is second line"
+    "This is third line"
+]  
+with open("demo.txt",'w') as file:
+    file.writelines(line)
+print("successfully created")    
 
 # TRY & EXCEPT
 try:
